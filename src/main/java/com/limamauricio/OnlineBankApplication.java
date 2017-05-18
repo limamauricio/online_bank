dee7056330d163dff0f386d128f9be5b9d8a758f
limamauricio <mauriciolimab@gmail.com>
2017-05-18 20:36:53 -0300

    project cfg

---
+++ src/main/java/com/limamauricio/OnlineBankApplication.java
@@ -0,0 +1,12 @@
+package com.limamauricio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankApplication.class, args);
+	}
+}
