b7ee2c0fd475a647c2a97f3ebc94e2e63669521f
limamauricio <mauriciolimab@gmail.com>
2017-05-18 21:46:24 -0300

    add hibernate cnf

--- src/main/java/com/limamauricio/controller/HomeController.java
+++ src/main/java/com/limamauricio/controller/HomeController.java
@@ -38,24 +38,24 @@ public class HomeController {
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public String signupPost(@ModelAttribute("user") User user, Model model) {
 
-        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
-
-            if (userService.checkEmailExists(user.getEmail())) {
-                model.addAttribute("emailExists", true);
-            }
-
-            if (userService.checkUsernameExists(user.getUsername())) {
-                model.addAttribute("usernameExists", true);
-            }
-
-            return "signup";
-        } else {
-            Set<UserRole> userRoles = new HashSet<>();
-            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
-
-            userService.createUser(user, userRoles);
+//        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+//
+//            if (userService.checkEmailExists(user.getEmail())) {
+//                model.addAttribute("emailExists", true);
+//            }
+//
+//            if (userService.checkUsernameExists(user.getUsername())) {
+//                model.addAttribute("usernameExists", true);
+//            }
+//
+//            return "signup";
+//        } else {
+//            Set<UserRole> userRoles = new HashSet<>();
+//            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+//
+//            userService.createUser(user, userRoles);
 
             return "redirect:/";
-        }
+ //       }
     }
 }
